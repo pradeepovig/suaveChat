63ae80b7dc04a37552020193a81ec8bfc0204b67
jatin2692 <sharmajatin440@gmail.com>
2023-07-26 15:49:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){});
 
 
+
   // video play code
 let play_pause_status = "play";
 function play_pause() {
-	var video_screen = document.getElementById('video-player');
+	let video_screen = document.getElementById('video-player');
 	if (play_pause_status == "pause") {
 		video_screen.play();
 		play_pause_status = "play";
